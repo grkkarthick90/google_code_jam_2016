ca49352df492a2a15aea42d1b4902ec2f8b13c23
Karthick GRK <karthick.krishna@realimage.com>
2017-04-08 10:48:25 +0530

    Hand the large input set

--- TidyNumbers.py
+++ TidyNumbers.py
@@ -1,17 +1,22 @@
-input = """4
-132
-1000
-7
-13216
-"""
-
-lines = input.splitlines()
+import numpy
+lines = open('small.input', 'r').read().splitlines()
 numberOfCases = int(lines[0])
 
-for number in range(numberOfCases):
-    findTidy = lines[number+1]
-    while True:
-        if list(findTidy) == sorted(list(findTidy)):
-            print 'Case #{}:'.format(number+1), findTidy
-            break
-        findTidy = str(long(findTidy) - 1)
+with open('output.txt', 'w') as out:
+    for number in range(numberOfCases):
+        findTidy = lines[number+1]
+        while True:
+            tidyList = numpy.array(list(findTidy))
+            lenth = len(tidyList)
+            allNotSet = False
+            for i in range(1, lenth):
+                if not tidyList[i-1] <= tidyList[i]:
+                    tidyList[i-1] = int(tidyList[i-1]) - 1
+                    tidyList[i:] = 9
+                    allNotSet = True
+                    findTidy = list(tidyList)
+                    break
+            if not allNotSet:
+                break
+
+        print 'Case #{}: {}'.format(number+1, int(''.join(tidyList)))
